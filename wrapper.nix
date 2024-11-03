7ac2ef5adfb18f346e4e70b3f2b5727809d3f7d4
turtton <top.gear7509@turtton.net>
2024-11-03 17:06:37 +0900

    Replace packaging method

--- wrapper.nix
+++ wrapper.nix
@@ -3,7 +3,13 @@ let
   plugins = import ./plugins { inherit pkgs; };
   nvimConfig = pkgs.callPackage ./config.nix { inherit plugins; };
 in
-pkgs.writeShellScriptBin "nvim" ''
-  PATH=$PATH:${pkgs.lib.makeBinPath extraPackages}
-  MY_CONFIG_PATH=${nvimConfig} ${pkgs.neovim-unwrapped}/bin/nvim -u ${nvimConfig}/init.vim "$@"
-''
+pkgs.writeShellApplication {
+  name = "nvim";
+  meta = {
+    description = "My Neovim configuration";
+  };
+  text = ''
+    		PATH=$PATH:${pkgs.lib.makeBinPath extraPackages}
+      	MY_CONFIG_PATH=${nvimConfig} ${pkgs.neovim-unwrapped}/bin/nvim -u ${nvimConfig}/init.vim "$@"
+    	'';
+}
