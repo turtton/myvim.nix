9bc8d9bec6bdcfb7c2ff3c5e38c36e3b747ef3c2
turtton <top.gear7509@turtton.net>
2025-01-01 15:20:19 +0900

    🐛 Fix opts type

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -120,50 +120,48 @@ return {
 			{ name = "nvim-web-devicons", dir = "@nvim_web_devicons@" },
 			{ name = "nui.nvim",          dir = "@nui_nvim@" },
 		},
-		opts = function()
-			return {
-				close_if_last_window = true,
-				window = {
-					width = 30,
-					mappings = {
-						["<space>"] = false,
+		opts = {
+			close_if_last_window = true,
+			window = {
+				width = 30,
+				mappings = {
+					["<space>"] = false,
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+				},
+				follow_curent_file = {
+					enabled = true,
+					leave_dirs_open = true,
+				},
+			},
+			default_component_configs = {
+				indent = { padding = 0 },
+				icon = {
+					folder_closed = get_icon("FolderClosed"),
+					folder_open = get_icon("FolderOpen"),
+					folder_empty = get_icon("FolderEmpty"),
+					folder_empty_open = get_icon("FolderEmpty"),
+					default = get_icon("DefaultFile"),
+				},
+				modified = { symbol = get_icon("FileModified") },
+				git_status = {
+					symbols = {
+						added = get_icon("GitAdd"),
+						deleted = get_icon("GitDelete"),
+						modified = get_icon("GitChange"),
+						renamed = get_icon("GitRenamed"),
+						untracked = get_icon("GitUntracked"),
+						ignored = get_icon("GitIgnored"),
+						unstaged = get_icon("GitUnstaged"),
+						staged = get_icon("GitStaged"),
+						conflict = get_icon("GitConflict"),
 					},
 				},
-				filesystem = {
-					filtered_items = {
-						hide_dotfiles = false,
-					},
-					follow_curent_file = {
-						enabled = true,
-						leave_dirs_open = true,
-					},
-				},
-				default_component_configs = {
-					indent = { padding = 0 },
-					icon = {
-						folder_closed = get_icon("FolderClosed"),
-						folder_open = get_icon("FolderOpen"),
-						folder_empty = get_icon("FolderEmpty"),
-						folder_empty_open = get_icon("FolderEmpty"),
-						default = get_icon("DefaultFile"),
-					},
-					modified = { symbol = get_icon("FileModified") },
-					git_status = {
-						symbols = {
-							added = get_icon("GitAdd"),
-							deleted = get_icon("GitDelete"),
-							modified = get_icon("GitChange"),
-							renamed = get_icon("GitRenamed"),
-							untracked = get_icon("GitUntracked"),
-							ignored = get_icon("GitIgnored"),
-							unstaged = get_icon("GitUnstaged"),
-							staged = get_icon("GitStaged"),
-							conflict = get_icon("GitConflict"),
-						},
-					},
-				},
-			}
-		end,
+			},
+		},
 		keys = {
 			{ "<leader>e", "<cmd>Neotree<cr>", desc = "Toggle file explorer" },
 		}
