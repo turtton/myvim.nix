92e2101e9fff02a9c037ed2f9fb37cc71c3e36e0
turtton <top.gear7509@turtton.net>
2025-09-21 13:36:53 +0900

    🔧 Add period change keybindings

--- nvim/lua/plugins/skk.lua
+++ nvim/lua/plugins/skk.lua
@@ -1,28 +1,32 @@
 return {
-	name = "skkeleton",
-	dir = "@skkeleton@",
-	lazy = false,
-	dependencies = {
-		{ name = "denops.vim", dir = "@denops_vim@" },
-	},
-	init = function()
-		vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-enable)")
-		vim.keymap.set("c", "<C-j>", "<Plug>(skkeleton-enable)")
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "skkeleton-initialize-pre",
-			callback = function()
-				vim.fn["skkeleton#config"]({
-					eggLikeNewline = true,
-					registerConvertResult = true,
-					globalDictionaries = {
-						"@skk_dict@",
-					},
-				})
-			end,
-		})
-	end,
-	config = function()
-		vim.api.nvim_eval("add(g:skkeleton#mapped_keys, '<Tab>')")
-		vim.fn["skkeleton#register_keymap"]("henkan", "<Tab>", "kakutei")
-	end,
+    name = "skkeleton",
+    dir = "@skkeleton@",
+    lazy = false,
+    dependencies = {
+        { name = "denops.vim", dir = "@denops_vim@" },
+    },
+    init = function()
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "skkeleton-initialize-pre",
+            callback = function()
+                vim.fn["skkeleton#config"]({
+                    eggLikeNewline = true,
+                    registerConvertResult = true,
+                    globalDictionaries = {
+                        "@skk_dict@",
+                    },
+                })
+            end,
+        })
+    end,
+    config = function()
+        vim.api.nvim_eval("add(g:skkeleton#mapped_keys, '<Tab>')")
+        vim.fn["skkeleton#register_keymap"]("henkan", "<Tab>", "kakutei")
+    end,
+    keys = {
+        { "<C-j>", "<Plug>(skkeleton-enable)", mode = "i", desc = "enable skkeleton" },
+        { "<C-j>", "<Plug>(skkeleton-enable)", mode = "c", desc = "enable skkeleton" },
+        { "<C-,>", "<C-o>:call skkeleton#register_kanatable('rom', {',': ['，', ''], '.' : ['．', '']})<CR><Plug>(skkeleton-enable)", mode = "i", desc = "skkeleton: , to ，" },
+        { "<C-.>", "<C-o>:call skkeleton#register_kanatable('rom', {',': [',', ''], '.' : ['.', '']})<CR><Plug>(skkeleton-enable)", mode = "i", desc = "skkeleton: ． to ." },
+    },
 }
